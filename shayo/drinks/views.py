19f2d66d6b6a66ade1c8c9aa129184f1ac6db6d8
jozefajogwu <jozefajogwu@gmail.com>
2023-10-19 17:46:09 +0100

    gET REQUEST NOT WORKING

--- shayo/drinks/views.py
+++ shayo/drinks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Drink
+from .serializers import DrinkSerializer
 
 # Create your views here.
+def drink_list(request):
+
+  #get all the drinks
+  drinks = Drink.objects(all)
+  serializer = DrinkSerializer(drinks, many=True)
+  return JsonResponse(serializer.data, safe=False)
+  
